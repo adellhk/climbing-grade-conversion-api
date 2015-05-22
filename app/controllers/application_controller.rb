7a1dda2ccbeb671e7667320a11a2924f3b9dbd98
Adell Hanson-Kahn <adell@adell.io>
2015-05-22 13:10:02 -0700

    tried to fix access-control-allow-origin on heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
 	after_filter :cors_set_access_control_headers
 	
 	def cors_set_access_control_headers
-		headers['Access-Control-Allow-Origin'] = '*'
+		headers['Access-Control-Allow-Origin'] = 'http://rockshoe.me'
 		headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
 		headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
 		headers['Access-Control-Max-Age'] = "1728000"
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::API
 	
 	def cors_preflight_check
 		if request.method == 'OPTIONS'
-			headers['Access-Control-Allow-Origin'] = '*'
+			headers['Access-Control-Allow-Origin'] = 'http://rockshoe.me'
 			headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
 			headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
 			headers['Access-Control-Max-Age'] = '1728000'
